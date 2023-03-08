31d13444a61314a40e607e458c9003681814c2f8
Jason Whittle <jason.whittle@gmail.com>
2023-03-08 18:33:59 -0500

    Initial Commit

---
+++ sg-fate/sg_fateApp.swift
@@ -0,0 +1,17 @@
+//
+//  sg_fateApp.swift
+//  sg-fate
+//
+//  Created by Jason Whittle on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct sg_fateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
